f7651e2105ffa251ed84da495c5bd3ab21d3a9a2
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2024-03-29 18:15:10 -0300

    shadcn uis and pages

---
+++ src/modules/auth/components/login-form.tsx
@@ -0,0 +1,44 @@
+import * as React from "react"
+ 
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
+export default function LoginForm() {
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Next auth</CardTitle>
+                <CardDescription>Faça login para continuar</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="email">E-mail</Label>
+                            <Input id="email" name="email" required />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="password">Senha</Label>
+                            <Input id="password" name="password" required />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button>Entrar</Button>
+                <Link href='/portal/cadastro' className={buttonVariants({ variant: 'link' })}>Criar conta</Link>
+            </CardFooter>
+        </Card>
+
+    )
+}
---
+++ src/modules/auth/components/signup-form.tsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+ 
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
+export default function SignUpForm() {
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Next auth</CardTitle>
+                <CardDescription>Preencha os campos abaixo para criar sua conta.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Nome</Label>
+                            <Input id="name" name="name" required />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="email">E-mail</Label>
+                            <Input id="email" name="email" required />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="password">Senha</Label>
+                            <Input id="password" name="password" required />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button>Criar conta</Button>
+                <Link href='/portal/login' className={buttonVariants({ variant: 'link' })}>Já tenho conta</Link>
+            </CardFooter>
+        </Card>
+
+    )
+}
